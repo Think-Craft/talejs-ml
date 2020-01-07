36fbb9a09c6b233b78524252f6f3a92d3123383b
Vagus-art <agustin.favo@gmail.com>
2020-01-07 00:57:17 -0300

    Changed ui a little bit, now looks like night-mode

--- src/components/Bots.js
+++ src/components/Bots.js
@@ -6,12 +6,12 @@ export default class Bots extends Component {
             <div class="view">
                 <h1>Our bots</h1>
                 <p></p>
-                <h3>Poebot</h3>
+                <h2>Poebot</h2>
                 <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquam ligula non hendrerit sagittis. Nullam rhoncus neque nec molestie mollis. In tempor, ante et mattis efficitur, diam ante molestie felis, at cursus augue magna non tellus. Ut consectetur ex vel risus varius malesuada. Vestibulum consectetur velit id feugiat cursus. Sed id nibh dui. Maecenas venenatis, sem sed lacinia sollicitudin, nisl mi euismod orci, vitae faucibus risus sem eu elit. In ut lectus sit amet eros eleifend sagittis. Duis a sapien sit amet nulla rutrum efficitur. In hac habitasse platea dictumst. Duis consequat accumsan elit. Donec ultricies luctus elementum. Aliquam erat volutpat.
                 Nam auctor nibh auctor dui.
                 </p>
-                <h3>H.P Robotcraft</h3>
+                <h2>H.P Robotcraft</h2>
                 <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquam ligula non hendrerit sagittis. Nullam rhoncus neque nec molestie mollis. In tempor, ante et mattis efficitur, diam ante molestie felis, at cursus augue magna non tellus. Ut consectetur ex vel risus varius malesuada. Vestibulum consectetur velit id feugiat cursus. Sed id nibh dui. Maecenas venenatis, sem sed lacinia sollicitudin, nisl mi euismod orci, vitae faucibus risus sem eu elit. In ut lectus sit amet eros eleifend sagittis. Duis a sapien sit amet nulla rutrum efficitur. In hac habitasse platea dictumst. Duis consequat accumsan elit. Donec ultricies luctus elementum. Aliquam erat volutpat.
                 Nam auctor nibh auctor dui.
