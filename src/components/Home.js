60b8cc94de413c0b948a8c9e806af337f1fcd504
Vagus-art <agustin.favo@gmail.com>
2020-01-07 01:41:37 -0300

    -

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,8 +7,6 @@ export default class Home extends Component {
                 <h1>Welcome to talejs!</h1>
                 <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor nisi nibh, et pretium urna viverra id. Morbi sem nunc, varius in nulla congue, pulvinar volutpat nibh. In nunc ante, finibus sit amet dignissim sed, malesuada et turpis. Phasellus ac rutrum diam. Quisque a neque accumsan, pulvinar diam eget, pulvinar tortor. Sed blandit ligula sit amet nisl placerat, non commodo ex suscipit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed eget tellus non nulla efficitur dictum fermentum in lectus. Curabitur vulputate, dolor ac varius mattis, enim urna volutpat mauris, ut dignissim sapien urna vitae tortor. Donec malesuada euismod efficitur.
-
-                Quisque semper augue sed augue sagittis pharetra. Integer dignissim maximus urna at dictum. Maecenas iaculis est non mauris euismod egestas. Aliquam semper, ipsum id volutpat facilisis, ante mauris efficitur risus, et pretium velit metus sit amet ex. Vivamus pretium nisl lacus, nec lobortis dui sodales id. Duis pellentesque nisi et magna tincidunt, ac fringilla magna malesuada. Nulla facilisi.
                 </p>
                 <p></p>
                 <div className="generation-panel">
